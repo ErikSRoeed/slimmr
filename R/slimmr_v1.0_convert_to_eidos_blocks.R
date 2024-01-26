065cf744771a4301b117b883666cada49de80f31
Erik Sandertun Røed <e.roeed@uq.net.au>
2024-01-26 18:58:23 +0100

    Make convert_to_eidos_blocks more robust to source file formatting
    Also update for recent commits to EidosBlock, EidosLine

--- R/slimmr_v1.0_convert_to_eidos_blocks.R
+++ R/slimmr_v1.0_convert_to_eidos_blocks.R
@@ -7,32 +7,55 @@
 #' @returns A list of slimmr::EidosBlock objects.
 #'
 #' @include slimmr_v1.0_EidosBlock.R
+#' @include slimmr_v1.0_EidosComposition.R
 #' @noRd
 #'
 convert_to_eidos_blocks <- function(eidos_lines)
 {
   stopifnot(is.list(eidos_lines))
 
-  test_if_eidos_line <- function(object) "EidosLine" %in% class(object)
-  inputs_are_valid <- sapply(eidos_lines, test_if_eidos_line)
+  check_eidos_line <- function(object) "EidosLine" %in% class(object)
+  inputs_are_valid <- sapply(eidos_lines, check_eidos_line)
 
   stopifnot(all(inputs_are_valid))
 
-  blocks <- list()
+  empty_first_block <- EidosBlock$new(items = list())
+  blocks <- list(empty_first_block)
+
+  current_block_number <- 1
+  must_add_new_block <- FALSE
 
   for (line in eidos_lines)
   {
-    last_block_number <- length(blocks)
-    line_is_callback_of_new_block <- ! is.null(line$callback)
+    possibly_add_new_block <- line$is_comment || line$is_callback
 
-    if (line_is_callback_of_new_block)
+    if (possibly_add_new_block)
     {
-      new_block_number <- last_block_number + 1
-      blocks[[new_block_number]] <- EidosBlock$new(eidos_lines = list(line))
+      must_add_new_block <- TRUE
+    }
+
+    current_block_needs_callback <- blocks[[current_block_number]]$callback |> is.null()
+
+    if (current_block_needs_callback)
+    {
+      must_add_new_block <- FALSE
+    }
+
+    not_add_new_block <- ! line$is_toplevel
+
+    if (not_add_new_block)
+    {
+      must_add_new_block <- FALSE
+    }
+
+    if (must_add_new_block)
+    {
+      current_block_number <- current_block_number + 1
+      blocks[[current_block_number]] <- EidosBlock$new(items = list(line))
       next
     }
 
-    blocks[[last_block_number]]$add_line(line)
+    blocks[[current_block_number]]$add(line)
   }
 
   return(blocks)
